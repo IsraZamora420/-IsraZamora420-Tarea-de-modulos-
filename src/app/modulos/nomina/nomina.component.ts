df94b1f3f22647d2a47d7024b321e2cd38cf0cbc
IsraZamora420 <israzamo36@gmail.com>
2024-09-23 22:15:13 -0500

    Primer commit

---
+++ src/app/modulos/nomina/nomina.component.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EstudiantesComponent } from './componentes/subcomponentes/ingreso-estudiantes/ingreso-estudiantes.component';
+import { RepresentantesComponent } from './componentes/subcomponentes/ingreso-representantes/ingreso-representantes.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    
+  ]
+})
+export class ModuloIngresoEstudiantesModule { }
